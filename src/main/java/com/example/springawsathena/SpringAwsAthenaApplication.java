bc4bb1b7fcf7ede41418de331dea193956279c9c
sieunkim <sieunkr@gmail.com>
2021-09-03 14:31:38 +0900

    Athena 연동 테스트

---
+++ src/main/java/com/example/springawsathena/SpringAwsAthenaApplication.java
@@ -0,0 +1,127 @@
+package com.example.springawsathena;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.athena.AthenaClient;
+import software.amazon.awssdk.services.athena.model.*;
+import software.amazon.awssdk.services.athena.paginators.GetQueryResultsIterable;
+
+import java.util.List;
+
+@SpringBootApplication
+public class SpringAwsAthenaApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAwsAthenaApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        //https://docs.aws.amazon.com/ko_kr/athena/latest/ug/code-samples.html
+
+        AwsBasicCredentials awsCreds = AwsBasicCredentials.create(
+                "",
+                "");
+
+        AthenaClient athenaClient = AthenaClient.builder()
+                .region(Region.AP_NORTHEAST_2)
+                .credentialsProvider(StaticCredentialsProvider.create(awsCreds))
+                .build();
+
+        try {
+            // The QueryExecutionContext allows us to set the database
+            QueryExecutionContext queryExecutionContext = QueryExecutionContext.builder()
+                    .database("sampledb").build();
+
+            // The result configuration specifies where the results of the query should go
+            ResultConfiguration resultConfiguration = ResultConfiguration.builder()
+                    .outputLocation("")
+                    .build();
+
+            StartQueryExecutionRequest startQueryExecutionRequest = StartQueryExecutionRequest.builder()
+                    .queryString("SELECT * FROM elb_logs limit 10;")
+                    .queryExecutionContext(queryExecutionContext)
+                    .resultConfiguration(resultConfiguration)
+                    .build();
+
+            StartQueryExecutionResponse startQueryExecutionResponse = athenaClient.startQueryExecution(startQueryExecutionRequest);
+            String queryExecutionId = startQueryExecutionResponse.queryExecutionId();
+
+            waitForQueryToComplete(athenaClient, queryExecutionId);
+
+            processResultRows(athenaClient, queryExecutionId);
+            athenaClient.close();
+
+            //System.out.println(result);
+
+        } catch (AthenaException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        System.out.println("test");
+    }
+
+    public static void waitForQueryToComplete(AthenaClient athenaClient, String queryExecutionId) throws InterruptedException {
+        GetQueryExecutionRequest getQueryExecutionRequest = GetQueryExecutionRequest.builder()
+                .queryExecutionId(queryExecutionId).build();
+
+        GetQueryExecutionResponse getQueryExecutionResponse;
+        boolean isQueryStillRunning = true;
+        while (isQueryStillRunning) {
+            getQueryExecutionResponse = athenaClient.getQueryExecution(getQueryExecutionRequest);
+            String queryState = getQueryExecutionResponse.queryExecution().status().state().toString();
+            if (queryState.equals(QueryExecutionState.FAILED.toString())) {
+                throw new RuntimeException("The Amazon Athena query failed to run with error message: " + getQueryExecutionResponse
+                        .queryExecution().status().stateChangeReason());
+            } else if (queryState.equals(QueryExecutionState.CANCELLED.toString())) {
+                throw new RuntimeException("The Amazon Athena query was cancelled.");
+            } else if (queryState.equals(QueryExecutionState.SUCCEEDED.toString())) {
+                isQueryStillRunning = false;
+            } else {
+                // Sleep an amount of time before retrying again
+                //Thread.sleep(ExampleConstants.SLEEP_AMOUNT_IN_MS);
+            }
+            System.out.println("The current status is: " + queryState);
+        }
+    }
+
+
+    public static void processResultRows(AthenaClient athenaClient, String queryExecutionId) {
+        try {
+            // Max Results can be set but if its not set,
+            // it will choose the maximum page size
+            GetQueryResultsRequest getQueryResultsRequest = GetQueryResultsRequest.builder()
+                    .queryExecutionId(queryExecutionId)
+                    .build();
+
+            GetQueryResultsIterable getQueryResultsResults = athenaClient.getQueryResultsPaginator(getQueryResultsRequest);
+
+            for (GetQueryResultsResponse result : getQueryResultsResults) {
+                List<ColumnInfo> columnInfoList = result.resultSet().resultSetMetadata().columnInfo();
+                List<Row> results = result.resultSet().rows();
+                processRow(results, columnInfoList);
+            }
+
+        } catch (AthenaException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    private static void processRow(List<Row> row, List<ColumnInfo> columnInfoList) {
+
+        for (Row myRow : row) {
+            List<Datum> allData = myRow.data();
+            for (Datum data : allData) {
+                System.out.println("The value of the column is "+data.varCharValue());
+            }
+        }
+    }
+
+}
---
+++ src/test/java/com/example/springawsathena/SpringAwsAthenaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springawsathena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAwsAthenaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
